d19d8ed20bbe68742a00159b6799ca4003ca0bd6
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-16 19:04:26 -0700

    Move svg files to src folder

---
+++ src/constants/images.js
@@ -0,0 +1,13 @@
+import iconFacebook from "../assets/icon-facebook.svg";
+import iconInstagram from "../assets/icon-instagram.svg";
+import iconPinterest from "../assets/icon-pinterest.svg";
+import iconTwitter from "../assets/icon-twitter.svg";
+import iconYoutube from "../assets/icon-youtube.svg";
+
+export default {
+  iconFacebook,
+  iconInstagram,
+  iconPinterest,
+  iconTwitter,
+  iconYoutube,
+};
