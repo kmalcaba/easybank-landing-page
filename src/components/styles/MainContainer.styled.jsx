9aca4084cbb7dfafec38f1ef48509920caa5e8e6
Kirsten Malcaba <kmalcaba@gmail.com>
2022-08-17 18:59:15 -0700

    Make container responsive

--- src/components/styles/MainContainer.styled.jsx
+++ src/components/styles/MainContainer.styled.jsx
@@ -5,4 +5,11 @@ export const MainContainer = styled.div`
   max-width: 100%;
   padding: 0 165px;
   margin: 0 auto;
+
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.desktop}) {
+    padding: 0 100px;
+  }
+  @media screen and (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    padding: 0 50px;
+  }
 `;
